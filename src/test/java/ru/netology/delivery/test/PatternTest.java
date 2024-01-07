f0b7e92c2e9aadbe4d5ceb7303b2ff73ca3b88a4
Gleb <Sephirothash@yandex.ru>
2024-01-07 16:16:00 +0300

    last

--- src/test/java/ru/netology/delivery/test/PatternTest.java
+++ src/test/java/ru/netology/delivery/test/PatternTest.java
@@ -38,18 +38,18 @@ class DeliveryTest {
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).click();
-        $("[data-test-id=success-notification].notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно запланирована на" + firstMeetingDate))
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate))
                         .shouldBe(Condition.visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME),Keys.DELETE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
-        $("[data-test-id=replan-notification].notification__content")
+        $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(Condition.visible);
-        $("[data-test-id=replan-notification] button").click();
-        $("[data-test-id=success-notification].notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно запланирована на" + firstMeetingDate))
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(Condition.visible);
     }
 }
